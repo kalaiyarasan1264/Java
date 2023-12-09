0add27b25649ffab128b314e2973f7ccf2d92fb2
Kalaiyarasan S <104194850+kalaiyarasan1264@users.noreply.github.com>
2023-12-10 00:36:49 +0530

    Commit

---
+++ Basic/hello.java
@@ -0,0 +1,8 @@
+
+
+public class hello{
+    public static void main(String args[]){
+        System.out.println("hello, i am first");
+        System.out.println("this is second line");
+    }
+}
---
+++ Strings/example.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class example {
+    public static void main(String[] args) {
+        String s= "Hi, this is the string";
+        System.out.println(s);
+    }
+}
